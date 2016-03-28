5f0d0cebfcff2c90478312a91172fd6e8bf421c6
Sara <sary.242@gmail.com>
2016-03-28 19:42:07 +0200

    Optimisations in code for first list

--- product.py
+++ product.py
@@ -1,31 +1,15 @@
 #a!/usr/bin/env python
 
-def product(*allvar):
-    nList = len(allvar)
-
-    i = 0
-    for elem1 in allvar[i]:
-        mylist =[]
-        mylist.append(elem1)
-        for j in xrange(i+1,nList):
-            myNewList = []
-            for nb in xrange(len(mylist)):
-                for x in xrange(len(allvar[j])):
-                    if j==i+1:
-                        myNewList.append([mylist[nb]])  
-                    else:
-                        myNewList.append(mylist[nb][:])  
-            mylist = myNewList[:]
-
-            for nbelem2, elem2 in enumerate(allvar[j]):
-                for val in xrange(nbelem2, len(mylist), len(allvar[j])):
-                    mylist[val].append(elem2)
-        for elem in mylist:
-            yield tuple(elem)
+def product(*lists):
+    result = [[]]
+    for next_list in lists:
+        result = [e + [next_elem] for next_elem in next_list for e in result]
+    for elem in result:
+        yield tuple(elem)
 
 
 if __name__ == '__main__':
-    var1 = [1,2,3]
+    var1 = [1, 2, 3]
     var2 = ["a", "b"]
     var3 = ["x", "y", "z"]
     var4 = [9,8,7,6,5]
